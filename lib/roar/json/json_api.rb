c2e2ed169b88e29a88e02f0ad1d5f63490b41915
Alex Coles <alex@alexbcoles.com>
2016-12-14 20:08:32 +0000

    Fix 1.9.3-compat.: include is private
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- lib/roar/json/json_api.rb
+++ lib/roar/json/json_api.rb
@@ -35,7 +35,7 @@ module Roar
             def self.meta(options={}, &block)
               representable_attrs[:meta_representer] ||= begin
                 meta_representer = Class.new(Roar::Decorator)
-                meta_representer.include(Roar::JSON)
+                meta_representer.send :include, Roar::JSON
                 meta_representer
               end
               representable_attrs[:meta_representer].instance_exec(&block)
@@ -95,7 +95,7 @@ module Roar
 
           representable_attrs[:meta_representer] ||= begin
             meta_representer = Class.new(Roar::Decorator)
-            meta_representer.include(Roar::JSON)
+            meta_representer.send :include, Roar::JSON
             meta_representer
           end
           representable_attrs[:meta_representer].instance_exec(&block)
