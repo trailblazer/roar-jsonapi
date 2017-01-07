1bb80cf96677361aa23fb399e62d5c72642bff48
Alex Coles <alex@alexbcoles.com>
2017-01-07 18:53:46 +0000

    Add json_spec-based assert_equal_json, colour diff
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,3 +10,33 @@ require 'representable/debug'
 require 'pp'
 
 require_relative 'jsonapi/representer'
+
+require 'json_spec/configuration'
+require 'json_spec/helpers'
+require 'json_spec/exclusion'
+
+if system('colordiff', __FILE__, __FILE__)
+  MiniTest::Assertions.diff = 'colordiff -u'
+end
+
+module JsonSpec
+  extend Configuration
+
+  self.excluded_keys = []
+end
+module MiniTest::Assertions
+  def assert_equal_json(actual, expected)
+    assert_equal scrub(actual), scrub(expected)
+  end
+
+  def scrub(json, path = nil)
+    JsonSpec::Helpers.generate_normalized_json(
+      JsonSpec::Exclusion.exclude_keys(
+        JsonSpec::Helpers.parse_json(json, path)
+      )
+    ).chomp + "\n"
+  end
+end
+module Minitest::Expectations
+  infect_an_assertion :assert_equal_json, :must_equal_json
+end
