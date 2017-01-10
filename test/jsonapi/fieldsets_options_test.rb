b8317adf1912eca1fba8cfe39622cf131b06356b
Alex Coles <alex@alexbcoles.com>
2017-01-10 09:12:38 +0000

    Flesh out fieldsets options test
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- test/jsonapi/fieldsets_options_test.rb
+++ test/jsonapi/fieldsets_options_test.rb
@@ -29,8 +29,16 @@ class FieldsetsOptionsTest < Minitest::Spec
     end
   end
 
+  describe 'with empty :include option' do
+    it 'rewrites :include' do
+      options = Include.({ include: [] }, {})
+      options.must_equal(include:  [:id, :attributes, :relationships, :included],
+                         included: { include: [] })
+    end
+  end
+
   describe 'with empty :include option and non-empty :fields option' do
-    it 'parses :fields option (_self), but does not include other resources' do
+    it 'parses :fields (_self), but does not include other resources' do
       options = Include.({
                            include: [],
                            fields:  { articles: [:title, :body], people: [] }
@@ -49,7 +57,7 @@ class FieldsetsOptionsTest < Minitest::Spec
                          relationships: { include: [:title, :body] })
     end
 
-    it 'parses :fields option, but does not include other resources' do
+    it 'parses :fields, but does not include other resources' do
       options = Include.({
                            include: [],
                            fields:  { articles: [:title, :body], people: [:email] }
@@ -73,7 +81,7 @@ class FieldsetsOptionsTest < Minitest::Spec
   end
 
   describe 'with non-empty :include option' do
-    it 'rewrites :include option: relationship name' do
+    it 'rewrites :include given a relationship name' do
       options = Include.({ include: ['comments'] }, {})
       options.must_equal(include:  [:id, :attributes, :relationships, :included],
                          included: {
@@ -85,7 +93,7 @@ class FieldsetsOptionsTest < Minitest::Spec
                          })
     end
 
-    it 'rewrites :include option: dot-separated path of relationship names' do
+    it 'rewrites :include given a dot-separated path of relationship names' do
       options = Include.({ include: ['comments.author.employer'] }, {})
       options.must_equal(include:  [:id, :attributes, :relationships, :included],
                          included: {
@@ -107,6 +115,32 @@ class FieldsetsOptionsTest < Minitest::Spec
                              _json_api_parsed: true
                            }
                          })
+    end
+
+    it 'does not rewrite :include if _json_api_parsed: true' do
+      options = Include.({ include:          [:id, :attributes],
+                           _json_api_parsed: true }, {})
+      options.must_equal(include:          [:id, :attributes],
+                         _json_api_parsed: true)
+    end
+  end
+
+  describe 'with falsey :include options' do
+    it 'does not rewrite include: false' do
+      options = Include.({ include: false }, {})
+      options.must_equal(include: false)
+    end
+
+    it 'does not rewrite include: nil' do
+      options = Include.({ include: nil }, {})
+      options.must_equal(include: nil)
+    end
+  end
+
+  describe 'with falsey :fields options' do
+    it 'does not parse fields: nil' do
+      options = Include.({ fields: nil }, {})
+      options.must_equal(fields: nil)
     end
   end
 end
